820deefe80e29216c6a3d1f1ed722a6ed485da32
zhaoshuai <13022199358@163.com>
2019-12-05 00:24:00 +0800

    springMVC入门小程序，基于注解和XML配置的两种形式 以及SpringMVC的运行流程及架构

--- springMVC_introduction/src/main/java/cn/zs/contoller/Hello2Controller.java
+++ springMVC_introduction/src/main/java/cn/zs/contoller/Hello2Controller.java
@@ -1,18 +1,20 @@
 package cn.zs.contoller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-public class HelloController  implements Controller {
+@org.springframework.stereotype.Controller
+public class Hello2Controller implements Controller {
+    @RequestMapping("/show.do")//此处可以写成show也可以写成show.do，都会被HandlerAdapter找到。
     @Override
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
         System.out.println("前端请求进来了.........");
         ModelAndView mv= new ModelAndView();
-        mv.setViewName("hello");
-        mv.addObject("msg","我是SpringMVC的第一个程序");
+        mv.setViewName("hello2");
+        mv.addObject("msg","这是一个基于springMVC注解的程序");
 
         return mv;
     }
